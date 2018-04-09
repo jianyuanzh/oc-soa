7ccc89df9c0f6dcaf8276720d24594d0777658fe
Jianyuan <vincent.zhang@logicmonitor.com>
2018-04-10 00:12:11 +0800

    user thrift service

---
+++ user-thrift-service/src/main/java/cc/databus/user/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package cc.databus.user.service;
+
+import cc.databus.thrift.user.UserInfo;
+import cc.databus.thrift.user.UserService;
+import cc.databus.user.mapper.UserMapper;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserServiceImpl implements UserService.Iface {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserInfo getUserById(int id) throws TException {
+        return userMapper.getUserById(id);
+    }
+
+    @Override
+    public UserInfo getUserByName(String name) throws TException {
+        return userMapper.getUserByName(name);
+    }
+
+    @Override
+    public void registerUser(UserInfo userinfo) throws TException {
+        userMapper.registerUser(userinfo);
+    }
+}
